aa49d5fdc22f10f2e8ffa87cbd16be75e07b711b
Guilherme Burle Medeiros <143973903+Guilherme-burle@users.noreply.github.com>
2025-11-20 17:06:19 -0300

    ExemploDAO.java etapa 5.1

--- src/br/edu/cs/poo/ac/ordem/daos/exemplo/ExemploDAO.java
+++ src/br/edu/cs/poo/ac/ordem/daos/exemplo/ExemploDAO.java
@@ -1,5 +1,6 @@
 package br.edu.cs.poo.ac.ordem.daos.exemplo;
 
+import br.edu.cs.poo.ac.utils.Registro;
 import java.io.Serializable;
 
 import br.edu.cs.poo.ac.ordem.daos.DAOGenerico;
@@ -10,46 +11,33 @@ import br.edu.cs.poo.ac.ordem.daos.DAOGenerico;
  *
  */
 public class ExemploDAO extends DAOGenerico {
-    public ExemploDAO() {
-        super(Exemplo.class);
+
+    @Override
+    public Class<?> getClasseEntidade() {
+        return Exemplo.class;
     }
+
     public Exemplo buscar(String codigo) {
-        return (Exemplo)cadastroObjetos.buscar(codigo);
+        return (Exemplo) super.buscar(codigo);
     }
+
     public boolean incluir(Exemplo exemplo) {
-        if (buscar(exemplo.getCodigo()) == null) {
-            cadastroObjetos.incluir(exemplo, exemplo.getCodigo());
-            return true;
-        } else {
-            return false;
-        }
+        return super.incluir(exemplo);
     }
+
     public boolean alterar(Exemplo exemplo) {
-        if (buscar(exemplo.getCodigo()) != null) {
-            cadastroObjetos.alterar(exemplo, exemplo.getCodigo());
-            return true;
-        } else {
-            return false;
-        }
+        return super.alterar(exemplo);
     }
+
     public boolean excluir(String codigo) {
-        if (buscar(codigo) != null) {
-            cadastroObjetos.excluir(codigo);
-            return true;
-        } else {
-            return false;
-        }
+        return super.excluir(codigo);
     }
+
     public Exemplo[] buscarTodos() {
-        Serializable[] ret = cadastroObjetos.buscarTodos();
-        Exemplo[] retorno;
-        if (ret != null && ret.length > 0) {
-            retorno = new Exemplo[ret.length];
-            for (int i=0; i<ret.length; i++) {
-                retorno[i] = (Exemplo)ret[i];
-            }
-        } else {
-            retorno = new Exemplo[0];
+        Registro[] registros = super.buscarTodos();
+        Exemplo[] retorno = new Exemplo[registros.length];
+        for (int i = 0; i < registros.length; i++) {
+            retorno[i] = (Exemplo) registros[i];
         }
         return retorno;
     }

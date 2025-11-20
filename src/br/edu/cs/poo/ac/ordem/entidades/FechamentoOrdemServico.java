a6cbcebba377ac8452b5952d052b25c9565f44d5
Guilherme Burle Medeiros <143973903+Guilherme-burle@users.noreply.github.com>
2025-11-20 17:16:47 -0300

    FechamentoOrdemServico.java etapa 5.1

--- src/br/edu/cs/poo/ac/ordem/entidades/FechamentoOrdemServico.java
+++ src/br/edu/cs/poo/ac/ordem/entidades/FechamentoOrdemServico.java
@@ -4,17 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import br.edu.cs.poo.ac.utils.Registro;
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class FechamentoOrdemServico implements Serializable {
+public class FechamentoOrdemServico implements Registro {
 
     private String numeroOrdemServico;
     private LocalDate dataFechamento;
     private boolean pago;
     private String relatorioFinal;
+
+    @Override
+    public String getId() {
+        return numeroOrdemServico;
+    }
 
 }
